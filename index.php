695fa4ecfba2aa4feceaf76e48e01f813c3e065e
Edinor Neto <edinorneto41@gmail.com>
2026-03-11 18:30:36 -0300

    feat: add base HTML form and destination fields

--- index.php
+++ index.php
@@ -7,5 +7,20 @@
     <link rel="stylesheet" href="">
 </head>
 <body>
+    <h1>Simulação de Regra de ICMS de SC - Com base na UF </h1>
+    <form action="process.php" method="get">
+        <label for="Digite para qual estado deseja consultar:"></label>
+        <input type="radio" name="UF" id="SC">
+        <label for="UF">SC</label>
+        <input type="radio" name="UF" id="PR">
+        <label for="UF">PR</label>
+        <input type="radio" name="UF" id="RS">
+        <label for="UF">RS</label>
+        <input type="radio" name="UF" id="MT">
+        <label for="UF">MT</label>
+        <input type="radio" name="UF" id="SP">
+        <label for="UF">SP</label>
+        <button type="submit">Consultar</button>
+    </form>
 </body>
 </html>
