06238127eeb23d2ba1cb6c8c8269f4055277b499
Edinor Neto <edinorneto41@gmail.com>
2026-03-19 17:45:22 -0300

    feat: add auto-ID, timestamp, JSON persistence, validation and error handling

--- process_cadastro.php
+++ process_cadastro.php
@@ -14,30 +14,49 @@ require_once 'data.php';
     $erros = [];
 
     if (empty($nome)) {
-
         $erros[] = "Nome é obrigatório.";
-
     }
     
     if (strlen($ncm) !== 8 || !ctype_digit($ncm)) {
-
         $erros[] = 'NCM inválido: deve conter exatamente 8 dígitos.';
-
     }
 
     if (!is_numeric($preco) || $preco <= 0) {
-
         $erros[] = 'Preço inválido.';
-
     }
 
     if (!is_numeric($estoque) || $estoque <= 0) {
-
         $erros[] = 'Estoque inválido.';
+    }
 
+    if (empty($un)) {
+        $erros[] = 'Unidade é obrigatória.';
     }
     
     if (empty($erros)) {
+        $produtos = carregar_produtos(ARQUIVO_JSON);
+
+        $ultimo_id = 0;
+        
+        foreach ($produtos as $p) {
+            if ($p['id'] > $ultimo_id) {
+                $ultimo_id = $p['id'];
+            }
+        }
+
+        $novo_produto = [
+            'id' => $proximo_id,
+            'nome' => $nome,
+            'categoria' => $categoria,
+            'ncm' => $ncm,
+            'preco' => floatval($preco),
+            'estoque' => floatval($estoque),
+            'unidade' => $un,
+            'ativo' => $ativo,
+            'data_cadastro' =>  date('d/m/Y H:i')
+        ];
+
+        $produtos[] = $novo_produto;
 
         salvar_produtos(ARQUIVO_JSON, $produtos);
 
@@ -49,5 +68,4 @@ require_once 'data.php';
             echo $erro . '<br>';
         }
     }
-
 ?>
