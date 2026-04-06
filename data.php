2b64b0dad759fbc2c015c295d86d417beecbc5e4
Edinor Neto <edinorneto41@gmail.com>
2026-04-06 12:12:01 -0300

    feat: add update function

--- data.php
+++ data.php
@@ -30,3 +30,39 @@ function salvar_produtos($caminho, $produtos) {
         return;
     }
 }
+
+function atualizar_produto($arquivo, $id, $novos_dados) { // recebe caminho do JSON, id que quero att e array com campos vindos do formulário
+    $dados = carregar_produtos($arquivo); // 1 - carregar le o arquivo json e retorna um array de produtos (minha funcao pronta, so indicar o que ela recebe)
+
+    foreach ($dados as $i => $produto) { // 2 - achar. para cada indice do array => produto
+        if ($id == $produto['id']) { // se o produto atual, é id que quero
+            $produto_atualizado = $produto; // atualizo, inicio o merge
+
+            foreach ($novos_dados as $campo => $valor) { // 3 - alterar. pra cada campo que veio do formulário 
+
+                if ($campo === 'id' || $campo === 'data_cadastro') continue; // se houve alteração em algum campo desse desconsidere
+
+                if (trim((string)$valor) === '') continue; // se ficar em branco desconsidere
+
+                if ($campo === 'preco' || $campo === 'estoque') { // se for preco ou estoque e o valor não for numérico desconsidere
+                    if (!is_numeric($valor)) continue;
+                    $produto_atualizado[$campo] = floatval($valor); // se nao, converta pra valor
+                    continue;
+                }
+
+                $produto_atualizado[$campo] = trim((string)$valor); // se for um campo sem ser os mencioados, salva o valor
+
+            }
+
+            $dados[$i] = $produto_atualizado; // faz a atualizacao a partir do id
+            salvar_produtos($arquivo, $dados); // salva
+            return $produto_atualizado; // retorna o valor atualizado
+
+        }
+    }
+    
+    return false; // se nao achar, retorna falso
+
+}
+
+?>
